7a21e2bb4a654944ac128a46a6af4a4223c14d90
Claudio La Rosa <96049337+Claudio-larosa@users.noreply.github.com>
2026-01-31 13:05:27 +0100

    feat: download button improvement

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,26 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import html2pdf from "html2pdf.js";
 import "swiper/swiper.min.css";
-import '../style/print.scss';
-
-// import $ from 'jquery';
-// import jsPDF from 'jspdf';
+import "../style/print.scss";
 
 export default function Resume() {
+  const [resume, setResume] = useState({
+    contatti: [],
+    educations: [],
+    experiences: [],
+    skills: [],
+    languages: [],
+    hobbies: [],
+  });
 
-  const [resume, setResume] = useState({ contatti: [], educations: [], experiences: [], skills: [], languages: [], hobbies: [] });
+  const savePDF = () => {
+    const source = document.body;
+    source.classList.add("print");
 
-  // var doc = new jsPDF();
-  // var specialElementHandlers = {
-  //   '#editor': function (element, renderer) {
-  //     return false;
-  //   }
-  // };
-
-  // const savePDF = () => {
-  //   doc.save('Claudio_La_Rosa_CV.pdf');
-  //   console.log(document.body);
-  // }
+    html2pdf()
+      .set({
+        filename: "Claudio La Rosa - CV.pdf",
+        margin: 0,
+        image: { type: "jpeg", quality: 1 },
+        html2canvas: { scale: 2 },
+        jsPDF: {
+          unit: "cm",
+          format: "A4",
+          orientation: "portrait",
+          putOnlyUsedFonts: true,
+        },
+      })
+      .from(document.body)
+      .save()
+      .then(function () {
+        source.classList.remove("print");
+      });
+  };
 
   useEffect(() => {
     fetchData();
@@ -33,36 +49,48 @@ export default function Resume() {
   }, []);
 
   const fetchData = () => {
-    Promise.all([
-      fetch(`/api/resume.json`)
-    ])
+    Promise.all([fetch(`/api/resume.json`)])
       .then(([res1]) => Promise.all([res1.json()]))
       .then(([data1]) => {
         setResume(data1);
       });
-  }
+  };
 
   return (
-    <div className="frame" id="pdf">
+    <div className="frame">
       <div className="container landing no-print">
         <h1>Resume</h1>
         <div className="selection">
-          <a href="#Education" className="button mini"><span>Education</span></a>
-          <a href="#Experiences" className="button mini"><span>Experiences</span></a>
-          <a href="#Design" className="button mini"><span>Design Skills</span></a>
-          <a href="#Languages" className="button mini"><span>Languages</span></a>
-          <a href="#Hobbies" className="button mini"><span>Hobbies</span></a>
+          <a href="#Education" className="button mini">
+            <span>Education</span>
+          </a>
+          <a href="#Experiences" className="button mini">
+            <span>Experiences</span>
+          </a>
+          <a href="#Design" className="button mini">
+            <span>Design Skills</span>
+          </a>
+          <a href="#Languages" className="button mini">
+            <span>Languages</span>
+          </a>
+          <a href="#Hobbies" className="button mini">
+            <span>Hobbies</span>
+          </a>
         </div>
       </div>
       <div className="landing only-print">
         <div className="propic">
-          <img src="/img/photo.jpg" title="claudio la rosa profile photo" />
+          <img
+            src="/img/photo.jpg"
+            title="claudio la rosa profile photo"
+            alt=""
+          />
         </div>
         <div className="dati">
           <h1>Claudio La Rosa</h1>
           <div className="contatti">
-            {resume.contatti.map(rec => (
-              <div className="single">
+            {resume.contatti.map((rec) => (
+              <div key={rec.data} className="single">
                 <i className={rec.icon}></i>
                 <span>{rec.data}</span>
               </div>
@@ -74,23 +102,31 @@ export default function Resume() {
         <div className="row print_nowrap">
           <div className="col-2">
             <h3 id="Education">Education</h3>
-            {resume.educations.map(edu => (
-              <div className="h-line">
+            {resume.educations.map((edu) => (
+              <div key={edu.name} className="h-line">
                 <h4>{edu.name}</h4>
                 <span className="period">{edu.period}</span>
                 <span className="cursus">{edu.cursus}</span>
-                <span className="learnt" dangerouslySetInnerHTML={{ __html: edu.learnt }}></span>
+                <span
+                  className="learnt"
+                  dangerouslySetInnerHTML={{ __html: edu.learnt }}
+                ></span>
               </div>
             ))}
           </div>
           <div className="col-2">
             <h3 id="Experiences">Experiences</h3>
-            {resume.experiences.map(xp => (
-              <div className="h-line">
+            {resume.experiences.map((xp) => (
+              <div key={xp.name} className="h-line">
                 <h4>{xp.name}</h4>
                 <span className="period">{xp.period}</span>
-                <span className="where"><i className="fas fa-map-marker-alt"></i> {xp.where}</span>
-                <span className="role" dangerouslySetInnerHTML={{ __html: xp.role }}></span>
+                <span className="where">
+                  <i className="fas fa-map-marker-alt"></i> {xp.where}
+                </span>
+                <span
+                  className="role"
+                  dangerouslySetInnerHTML={{ __html: xp.role }}
+                ></span>
               </div>
             ))}
           </div>
@@ -99,14 +135,20 @@ export default function Resume() {
           <div className="col">
             <h3 id="Design">Skills</h3>
             <div className="row print_nowrap">
-              {resume.skills.map(sec => (
-                <div className="col-3">
+              {resume.skills.map((sec) => (
+                <div key={sec.name} className="col-3">
                   <h4>{sec.name}</h4>
-                  {sec.skills.map(skill => (
-                    <div className="design">
+                  {sec.skills.map((skill) => (
+                    <div key={skill.name} className="design">
                       <div className="title">{skill.name}</div>
                       <div className={`dot n-${skill.level}`}>
-                        {[1,2,3,4,5].map(level => <i className={`${level <= skill.level ? "far" : "fad"} fa-microchip`}></i>)}
+                        {[1, 2, 3, 4, 5].map((level) => (
+                          <i
+                            className={`${
+                              level <= skill.level ? "fas" : "far"
+                            } fa-circle`}
+                          ></i>
+                        ))}
                       </div>
                     </div>
                   ))}
@@ -115,12 +157,16 @@ export default function Resume() {
             </div>
           </div>
         </div>
-        <div className="row print_nowrap">
+        <div className="row no-print">
           <div className="col center">
             <h3 id="Languages">Languages</h3>
-            <Swiper slidesPerView={"auto"} spaceBetween={30} className="mySwiper">
-              {resume.languages.map(lang => (
-                <SwiperSlide>
+            <Swiper
+              slidesPerView={"auto"}
+              spaceBetween={30}
+              className="mySwiper"
+            >
+              {resume.languages.map((lang) => (
+                <SwiperSlide key={lang.lang}>
                   <figure className={`chart-${lang.level} animate`}>
                     <svg role="img" xmlns="http://www.w3.org/2000/svg">
                       <title>{lang.lang}</title>
@@ -129,9 +175,7 @@ export default function Resume() {
                       <circle className="circle-foreground" />
                     </svg>
                     <figcaption>{lang.lang}</figcaption>
-                    <div className="level">
-                      {lang.desc}
-                    </div>
+                    <div className="level">{lang.desc}</div>
                   </figure>
                 </SwiperSlide>
               ))}
@@ -139,12 +183,12 @@ export default function Resume() {
           </div>
         </div>
       </div>
-      <div className="row hobbies">
+      <div className="row hobbies no-print">
         <div className="col center">
           <h3 id="Hobbies">Hobbies & Interests</h3>
           <Swiper slidesPerView={"auto"} className="mySwiper">
-            {resume.hobbies.map(hobbie => (
-              <SwiperSlide>
+            {resume.hobbies.map((hobbie) => (
+              <SwiperSlide key={hobbie.name}>
                 <div className="hobbie">
                   <i className={hobbie.icon}></i>
                   <span>{hobbie.name}</span>
@@ -154,11 +198,12 @@ export default function Resume() {
           </Swiper>
         </div>
       </div>
-      <div className="button mini print" onClick={() => window.print()}>
-      {/* <div className="button mini print" onClick={savePDF}> */}
-        <span>Print or Save</span>
-        <dot>.</dot><dot>.</dot><dot>.</dot>
+      <div className="button mini print" onClick={savePDF}>
+        <span>Download my CV</span>
+        <dot>.</dot>
+        <dot>.</dot>
+        <dot>.</dot>
       </div>
     </div>
   );
-};
+}
